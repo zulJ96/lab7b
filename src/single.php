4b5a1fb30cd6da7551c6d285e7b2bb29d09b9447
zulfadli bin johari <1902147@sit.singaporetech.edu.sg>
2021-12-02 06:43:33 +0000

    web

--- src/single.php
+++ src/single.php
@@ -1,7 +1,7 @@
 <?php
-echo "<form action="/action.php" method="POST">";
-echo "  <label for="name">name:</label><br>";
-echo "  <input type="text" id="name" name="name"><br>";
-echo "  <input type="submit" value="Submit">";
-echo "</form>"
+echo "<form action="/action.php" method="POST">;";
+echo "  <label for="name">name:</label><br>;";
+echo "  <input type="text" id="name" name="name"><br>;";
+echo "  <input type="submit" value="Submit">;";
+echo "</form>";
 ?>
